4d4993c3ffd28c92c2dc0d7997af71926df67c99
Emanuel Ramos <ramosg2e@gmail.com>
2023-12-22 19:00:46 -0300

    end!

--- js/main.js
+++ js/main.js
@@ -1,36 +1,3 @@
-// import { getAllCountries } from "./service.js";
-// window.onload = () => {
-//     loadCountries();
-// };
-
-// const loadCountries = () => {
-//     const dataContainer = 
-//         document.getElementById('data-container');
-    
-        
-//         getAllCountries().then(resp =>{
-//         resp.forEach(country => {
-//             console.log(country)
-//             const countryElement = 
-//                 document.createElement('div');
-//                 countryElement.innerHTML =`
-//                 <div style="border: solid 1px blue">
-//                     <div>
-//                         <img src="${country.flags.png}" alt="${country.flags.alt}">
-//                         <h2>
-//                             ${country.name.common}
-//                         </h2>
-//                     </div>
-//                     <hr>
-//                 </div>
-                
-//                 `;
-//              dataContainer.appendChild(countryElement);
-//         });
-//     })
-// };
-
-
 const question = document.querySelector(".question");
 const answers = document.querySelector(".answers");
 const spnQtd = document.querySelector(".spnQtd");
--- js/questions.js
+++ js/questions.js
@@ -36,4 +36,52 @@ export default [
             { option: "Alemão, Francês e Suíço", correct: false },
         ],
     },
+    {
+        question: {title: "Em qual país fica localizado a cidade de Macchu Picchu?"},
+        answers: [
+            { option: "Colombia", correct: false },
+            { option: "Peru", correct: true },
+            { option: "Venezuela", correct: false },
+        ],
+    },
+    {
+        question: {title: "Qual oceano banha o Brasil?"},
+        answers: [
+            { option: "Oceano Atlântico", correct: true },
+            { option: "Oceano Índico", correct: false },
+            { option: "Oceano Pacífico", correct: false },
+        ],
+    },
+    {
+        question: {title: "Jacarta é a capital de qual país?"},
+        answers: [
+            { option: "Japão", correct: false },
+            { option: "Nova-Zelândia", correct: false },
+            { option: "Indonésia", correct: true },
+        ],
+    },
+    {
+        question: {title: "Qual idioma é falado no Chipre"},
+        answers: [
+            { option: "Grego e Francês", correct: false },
+            { option: "Turco e Chipreano", correct: false },
+            { option: "Grego e Turco", correct: true },
+        ],
+    },
+    {
+        question: {title: "Qual desses 3 Países tem mais habitantes:"},
+        answers: [
+            { option: "Maldivas", correct: true },
+            { option: "Malta", correct: false  },
+            { option: "Bahamas", correct: false },
+        ],
+    },
+    {
+        question: {title: "Qual era a moeda oficial do Brasil antes do Real"},
+        answers: [
+            { option: "Cruzados", correct: false },
+            { option: "Cruzeiro", correct: true },
+            { option: "Ouro", correct: false },
+        ],
+    },
 ];
